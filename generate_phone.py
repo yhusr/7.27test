3cd9cc33706d8cb6e5326f79048e0791d70b40a1
yhusr <904239064@qq.com>
2020-07-28 14:03:38 +0800

    pytest模式

---
+++ generate_phone.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+# @Time    :2020-07-28 14:02
+# @Author  :toy_yh
+# @File    :generate_phone.py
+# @Software:PyCharm
+from scripts.handle_phone import HandlePhone
+
+if __name__ == '__main__':
+    HandlePhone.generate_phone()
